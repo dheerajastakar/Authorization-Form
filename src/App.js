850c61c84e3e36e0f0c8d05ba84238d74c5f84b7
dheerajastakar <dheerajastakar@gmail.com>
2022-02-01 16:32:39 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Contact from './Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Enter Password "swordfish"</h1>
+                <Contact/>
+                <Contact/>
+                <Contact/>
+                <Contact/>
+                <Contact/>
+                <Contact/>
+
+            </div>
+          
+        )
+    }
 }
 
 export default App;
